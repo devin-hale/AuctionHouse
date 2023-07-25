10343a43acab2d2d7daf6b6ae9f2e113dfb71d10
dwhale2 <devinwaynehale@gmail.com>
2023-07-25 16:50:14 -0500

    Progress

--- src/app/shop/[productId]/page.jsx
+++ src/app/shop/[productId]/page.jsx
@@ -3,9 +3,18 @@ import itemData from "./../../../data.JSON";
 import Image from "next/image";
 import NavBar from "@/app/components/navBar";
 import { useState } from "react";
+import { addProduct } from "@/app/components/cartSlice";
+import { useRouter } from "next/navigation";
 
 const ProductDetail = ({ params }) => {
+  const [added, setAdded] = useState(false);
   const itemDB = itemData.items;
+  const router = useRouter();
+
+  const routerPrefetch = () => {
+    router.prefetch(`/shop`);
+    router.push(`/shop`);
+  };
 
   const item = itemDB.find((item) => item.id == params.productId);
 
@@ -28,10 +37,15 @@ const ProductDetail = ({ params }) => {
     <>
       <div className=" p-2 border-2 bg-black border-white border-solid w-[80%] m-auto flex flex-col justify-between min-h-[400px]">
         <div>
-          <p className="text-[25px]" style={{ color: `${itemColor(item)}` }}>
-            {item.name}
-          </p>
-          <p className="text-[17px] text-[#FFFC01]">{item.ilevel}</p>
+          <button onClick={() => routerPrefetch()} type="button">
+            Back
+          </button>
+          <div>
+            <p className="text-[25px]" style={{ color: `${itemColor(item)}` }}>
+              {item.name}
+            </p>
+            <p className="text-[17px] text-[#FFFC01]">{item.ilevel}</p>
+          </div>
         </div>
 
         <div className="flex flex-col m-auto sm:flex-row">
@@ -54,36 +68,39 @@ const ProductDetail = ({ params }) => {
           </div>
         </div>
 
-        <div className=" text-[25px] flex flex-row flex-nowrap items-center m-auto w-fit justify-evenly border-solid border-white border-2">
-          <p className="mr-[10px]">Cost:</p>
-          <div className="flex flex-row flex-nowrap w-[50px] max-h-[20px] items-center align-middle justify-evenly">
-            <p>{item.priceG}</p>
-            <Image
-              className="m-auto"
-              height={20}
-              width={20}
-              src="/assets/goldIMG/Gold.webp"
-              alt="Gold"
-            />
+        <div className="flex flex-row flex-wrap m-auto items-center w-[100%] text-[25px] justify-evenly ">
+          <div className="flex flex-row flex-nowrap items-center justify-evenly border-solid border-white border-2">
+            <p className="mr-[10px]">Cost:</p>
+            <div className="flex flex-row flex-nowrap w-[50px] max-h-[20px] items-center align-middle justify-evenly">
+              <p>{item.priceG}</p>
+              <Image
+                className="m-auto"
+                height={20}
+                width={20}
+                src="/assets/goldIMG/Gold.webp"
+                alt="Gold"
+              />
+            </div>
+            <div className="flex flex-row flex-nowrap w-[50px] max-h-[20px] align-middle justify-evenly items-center">
+              <p>{item.priceS}</p>
+              <Image
+                height={10}
+                width={20}
+                src="/assets/goldIMG/Silver.webp"
+                alt="Silver"
+              />
+            </div>
+            <div className="flex flex-row flex-nowrap w-[50px] max-h-[20px] align-middle justify-evenly items-center">
+              <p>{item.priceC}</p>
+              <Image
+                height={10}
+                width={20}
+                src="/assets/goldIMG/Copper.webp"
+                alt="Copper"
+              />
+            </div>
           </div>
-          <div className="flex flex-row flex-nowrap w-[50px] max-h-[20px] align-middle justify-evenly items-center">
-            <p>{item.priceS}</p>
-            <Image
-              height={10}
-              width={20}
-              src="/assets/goldIMG/Silver.webp"
-              alt="Silver"
-            />
-          </div>
-          <div className="flex flex-row flex-nowrap w-[50px] max-h-[20px] align-middle justify-evenly items-center">
-            <p>{item.priceC}</p>
-            <Image
-              height={10}
-              width={20}
-              src="/assets/goldIMG/Copper.webp"
-              alt="Copper"
-            />
-          </div>
+          <button type="button">Add to Cart</button>
         </div>
       </div>
     </>
