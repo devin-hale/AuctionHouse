34fe5f833cc60cebc73c46f260ac772eeb4e8195
dwhale2 <devinwaynehale@gmail.com>
2023-07-27 09:58:10 -0500

    Cart page now shows taxes at 10%.

--- src/app/cart/costComponent.jsx
+++ src/app/cart/costComponent.jsx
@@ -29,8 +29,49 @@ export const CartCost = () => {
     return { totalG: totalG, totalS: totalS, totalC: totalC };
   };
 
+  const taxesTotal = (totalCopper) => {
+    return totalCopper * 0.1;
+  };
+
   return (
     <div>
+      <div className="flex flex-row items-center text-[20px] justify-end w-[85%] sm:w-[60%] m-auto">
+        <p>Taxes: </p>
+        <div className="flex flex-row flex-wrap sm:flex-nowrap items-center m-2 justify-evenly text-white">
+          <div className="flex flex-row flex-nowrap items-center max-h-[20px] align-middle justify-evenly">
+            <p>{totalGoldCostAddUp(taxesTotal(totalCartCopper)).totalG}</p>
+            <Image
+              className="m-auto"
+              height={20}
+              width={20}
+              src="/assets/goldIMG/Gold.webp"
+              alt="Gold"
+            />
+          </div>
+          <div className="flex flex-row flex-nowrap w-[50px] max-h-[20px] items-center align-middle justify-evenly">
+            <p>{totalGoldCostAddUp(taxesTotal(totalCartCopper)).totalS}</p>
+            <Image
+              height={10}
+              width={20}
+              src="/assets/goldIMG/Silver.webp"
+              alt="Silver"
+            />
+          </div>
+          <div className="flex flex-row flex-nowrap w-[50px] max-h-[20px] align-middle items-center justify-evenly">
+            <p>
+              {Math.floor(
+                totalGoldCostAddUp(taxesTotal(totalCartCopper)).totalC
+              )}
+            </p>
+            <Image
+              height={10}
+              width={20}
+              src="/assets/goldIMG/Copper.webp"
+              alt="Copper"
+            />
+          </div>
+        </div>
+      </div>
       <div className="flex flex-row items-center text-[20px] justify-end w-[85%] sm:w-[60%] m-auto">
         <p>Total: </p>
         <div className="flex flex-row flex-wrap sm:flex-nowrap items-center m-2 justify-evenly text-white">
